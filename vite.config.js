f9d679789759e408db66d99b37b6cee2b31921c3
domthecoder-dev <dominic3lliott@gmail.com>
2025-10-04 22:17:16 +0200

    Fix Blank Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wild-free-traveltribe./', // IMPORTANT: Replace with your repository name
+  base: './', // ← CHANGE TO THIS instead of '/your-repo-name/'
 })
