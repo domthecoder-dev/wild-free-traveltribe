c271f85cfe8c2ca9d32fa6b733c3365a71b5d732
domthecoder-dev <dominic3lliott@gmail.com>
2025-10-05 16:31:53 +0200

    Changes 1.0

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '', // ← CHANGE TO THIS instead of '/your-repo-name/'
+  base: '/wild-free-traveltribe/', // Keep this
 })
