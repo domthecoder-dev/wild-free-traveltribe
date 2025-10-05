2ce711a3f46b4b640391ccf782daa7d9c5b6fbd2
domthecoder-dev <dominic3lliott@gmail.com>
2025-10-05 21:46:20 +0200

    Changes
    1. UL added margins for mobile
    2. Contact detaisl drop a line on mobile

--- src/Sections/ContactUs-Files/ContactUs.jsx
+++ src/Sections/ContactUs-Files/ContactUs.jsx
@@ -7,8 +7,8 @@ function ContactUs() {
       <h1>Have More Questions?</h1>
       <h3>Feel Free to Contact Us!</h3>
       <ul>
-        <li>WhatsApp:<a href="https://wa.me/27827174772" target="_blank" alt="to travel agent's whatsapp"> +27 82 717 4772</a></li>
-        <li>Email Us:<a href="mailto: info@wildandfreezanzibar.com"> info@wildandfreezanzibar.com</a></li>
+        <li>WhatsApp:<br className="MobileBr"/><a href="https://wa.me/27827174772" target="_blank" alt="to travel agent's whatsapp"> +27 82 717 4772</a></li>
+        <li>Email Us:<br className="MobileBr"/><a href="mailto: info@wildandfreezanzibar.com"> info@wildandfreezanzibar.com</a></li>
       </ul>
     </div>
   )
