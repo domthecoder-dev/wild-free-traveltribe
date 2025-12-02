e5c90a522975b4aa0a5133d4abac6174b67069a9
domthecoder-dev <dominic3lliott@gmail.com>
2025-12-02 16:56:16 +0200

    Ght

--- src/Components/Footer-files/Footer.jsx
+++ src/Components/Footer-files/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import WFLogo from '../../assets/wildandfree-rec.svg'
+import TTSSPLogo from '../../Images/TTSSP.svg'
 import "./Footer.css"
 
 function Footer() {
@@ -16,7 +17,11 @@ function Footer() {
             />
         </div>
         <div className="Nav-Links">
-
+            <img 
+              src={TTSSPLogo}
+              alt="TTSSP Logo"
+              ClassName="TTSSPLogo"
+            />
         </div>
       </div>
       <p className="Credit-footer">&copy; 2025 Wild and Free Travel tribe |  Created by <br className="MobileBr"/><a className="CreditLink" href="https://www.deinnovativesolutions.co.za" target="_blank"><span className="Blue-letter">D</span><span className="Orange-letter">E</span><span className="Grey-Name"> Innovative Solutions</span></a></p>
